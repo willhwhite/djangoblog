fbf46a3e77ac2522e82b882e1095336ab7dc3666
Will <williamwhite0@gmail.com>
2019-04-28 22:11:58 -0700

    Lesson 7 Assignment 2: Designate a category from the post admin

--- mysite/myblog/admin.py
+++ mysite/myblog/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 from myblog.models import Post
 from myblog.models import Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class CategoryInline(admin.StackedInline):
+    model = Category.posts.through
+    exclude = ('posts',)
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+    ]
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
